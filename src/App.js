e73b9458dc126778b204dbf29fd831d45913d11f
anshu vishwakarma <anshuvishwakarma923@gmail.com>
2021-10-08 21:50:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './component/Home';
+import Main from './component/Main';
+import CheckOut from './component/CheckOut';
+import Error404 from './component/Error404';
+function App() {
+  return (
+    <div className="App">
+       
+        <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={CheckOut} />
+          <Route exact path="/Main" component={Main} />
+          <Route path="//////"  component={Error404} />
+        </Switch>
+      </BrowserRouter>
+     </div>
+    
+  );
+}
+
+export default App;
